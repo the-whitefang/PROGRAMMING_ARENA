aeca7df8a15f671e2b4fd0c7b3a2c7bda7c661d0
Abhilash <abhilashvenugopal11111@gmail.com>
2023-12-10 10:09:20 +0530

    done some code

---
+++ Python_Codes/p10.py
@@ -0,0 +1,6 @@
+# Program to convert Temperature in Celcius to Farenheit
+celsius = 47
+fahrenheit = (celsius * 1.8) + 32
+
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+	% (celsius, fahrenheit))

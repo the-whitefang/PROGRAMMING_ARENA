f9a41e45d1d1e559663244132efab8042434f22e
Abhilash <abhilashvenugopal11111@gmail.com>
2023-12-10 10:04:48 +0530

    done some code

---
+++ Java_Codes/min.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class min {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your values one after one: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a < b && a < c ){
+            System.out.println(a + ": is the Smallest Number.");
+        }
+        else if(b < c && b < a ){
+            System.out.println(b + ": is the Smallest Number.");
+        }
+        System.out.println(c + ": is the Smallest Number.");
+    }
+}
